7bf0c8823368ad109cdcfded8fb3c094505dcb6b
Araden <2713886816@qq.com>
2023-05-30 10:47:05 +0800

    feat: 添加i18n组件

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,14 @@ import '@arco-design/web-vue/dist/arco.css'
 
 import App from './App.vue'
 import { router } from './router'
+import { i18n } from './locale'
 import './style.css'
 
 const app = createApp(App)
+
 app.use(ArcoVue)
+
 app.use(router)
+app.use(i18n)
+
 app.mount('#app')
--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -2,9 +2,11 @@
   <div class="w-2/5 m-auto p-8 rounded-xl shadow-xl">
     <a-tabs default-active-key="1">
       <a-tab-pane key="1" title="Tab 1">
-        <router-link to="/file">登陆</router-link>
+        <router-link to="/file">{{ $t('login.title') }}</router-link>
       </a-tab-pane>
       <a-tab-pane key="2" title="Tab 2"> Content of Tab Panel 2 </a-tab-pane>
     </a-tabs>
   </div>
 </template>
+
+<script lang="ts" setup></script>
