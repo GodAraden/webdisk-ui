63da3f2f40443ad53e7a6070b4610317a8369c44
Araden <2713886816@qq.com>
2023-05-30 10:07:32 +0800

    feat: 添加tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import '@arco-design/web-vue/dist/arco.css'
 
 import App from './App.vue'
 import { router } from './router'
+import './style.css'
 
 const app = createApp(App)
 app.use(ArcoVue)
--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -1,3 +1,10 @@
 <template>
-  <div>登陆</div>
+  <div class="w-2/5 m-auto p-8 rounded-xl shadow-xl">
+    <a-tabs default-active-key="1">
+      <a-tab-pane key="1" title="Tab 1">
+        <router-link to="/file">登陆</router-link>
+      </a-tab-pane>
+      <a-tab-pane key="2" title="Tab 2"> Content of Tab Panel 2 </a-tab-pane>
+    </a-tabs>
+  </div>
 </template>
