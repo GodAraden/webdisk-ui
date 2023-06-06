9c340f54831d77f147c3b9c89d9596523a16c182
Araden <2713886816@qq.com>
2023-06-06 19:27:25 +0800

    style: card-view 加载中

--- src/views/file-list/components/card-view.vue
+++ src/views/file-list/components/card-view.vue
@@ -1,72 +1,77 @@
 <template>
-  <a-grid
-    :cols="7"
-    :colGap="24"
-    :rowGap="16"
+  <a-card
+    :bordered="false"
+    :loading="loading"
+    :body-style="{ padding: 0 }"
     :style="{ height: '70vh', overflowY: 'scroll' }"
-    class="grid-demo-grid"
   >
-    <a-grid-item v-for="item in renderData" :key="item.id">
-      <a-dropdown trigger="contextMenu" position="br" :unmount-on-close="false">
-        <a-card
-          :bordered="false"
-          hoverable
-          class="cursor-pointer select-none"
-          @dblclick="() => onDoubleClickFile(item)"
+    <a-grid :cols="7" :colGap="24" :rowGap="16" class="grid-demo-grid">
+      <a-grid-item v-for="item in renderData" :key="item.id">
+        <a-dropdown
+          trigger="contextMenu"
+          position="br"
+          :unmount-on-close="false"
         >
-          <template #cover>
-            <div class="w-12 h-12 mx-auto my-3 mb-0">
-              <img :src="`/icon/${typeToIcon(item.type)}.svg`" />
-            </div>
+          <a-card
+            :bordered="false"
+            hoverable
+            class="cursor-pointer select-none"
+            @dblclick="() => onDoubleClickFile(item)"
+          >
+            <template #cover>
+              <div class="w-12 h-12 mx-auto my-3 mb-0">
+                <img :src="`/icon/${typeToIcon(item.type)}.svg`" />
+              </div>
+            </template>
+            <p class="ellipsis" :title="item.name"> {{ item.name }} </p>
+          </a-card>
+          <template #content>
+            <a-doption @click="() => onDownloadFile(item.id)">
+              <icon-download />
+              {{ $t('filelist.contextMenu.download') }}
+            </a-doption>
+            <a-doption>
+              <icon-launch />
+              {{ $t('filelist.contextMenu.open') }}
+            </a-doption>
+            <a-doption>
+              <icon-share-alt />
+              {{ $t('filelist.contextMenu.share') }}
+            </a-doption>
+            <hr />
+            <a-doption>
+              <icon-copy />
+              {{ $t('filelist.contextMenu.copy') }}
+            </a-doption>
+            <a-doption>
+              <icon-scissor />
+              {{ $t('filelist.contextMenu.cut') }}
+            </a-doption>
+            <a-doption>
+              <icon-font-colors />
+              {{ $t('filelist.contextMenu.rename') }}
+            </a-doption>
+            <a-doption>
+              <icon-info-circle />
+              {{ $t('filelist.contextMenu.info') }}
+            </a-doption>
+            <hr />
+            <a-doption>
+              <icon-delete />
+              {{ $t('filelist.contextMenu.delete') }}
+            </a-doption>
           </template>
-          <p class="ellipsis" :title="item.name"> {{ item.name }} </p>
-        </a-card>
-        <template #content>
-          <a-doption @click="() => onDownloadFile(item.id)">
-            <icon-download />
-            {{ $t('filelist.contextMenu.download') }}
-          </a-doption>
-          <a-doption>
-            <icon-launch />
-            {{ $t('filelist.contextMenu.open') }}
-          </a-doption>
-          <a-doption>
-            <icon-share-alt />
-            {{ $t('filelist.contextMenu.share') }}
-          </a-doption>
-          <hr />
-          <a-doption>
-            <icon-copy />
-            {{ $t('filelist.contextMenu.copy') }}
-          </a-doption>
-          <a-doption>
-            <icon-scissor />
-            {{ $t('filelist.contextMenu.cut') }}
-          </a-doption>
-          <a-doption>
-            <icon-font-colors />
-            {{ $t('filelist.contextMenu.rename') }}
-          </a-doption>
-          <a-doption>
-            <icon-info-circle />
-            {{ $t('filelist.contextMenu.info') }}
-          </a-doption>
-          <hr />
-          <a-doption>
-            <icon-delete />
-            {{ $t('filelist.contextMenu.delete') }}
-          </a-doption>
-        </template>
-      </a-dropdown>
-    </a-grid-item>
-  </a-grid>
+        </a-dropdown>
+      </a-grid-item>
+    </a-grid>
+  </a-card>
 </template>
 
 <script lang="ts" setup>
 import { typeToIcon } from '@/utils/format'
 import { useFileList } from '../hooks/useFileList'
 
-const { renderData, onDownloadFile, onDoubleClickFile } = useFileList()
+const { loading, renderData, onDownloadFile, onDoubleClickFile } = useFileList()
 </script>
 
 <style lang="less" scoped>
