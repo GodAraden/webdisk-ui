9f4069be753c3554b59423e5f3a232601fde9734
Araden <2713886816@qq.com>
2023-06-07 21:03:09 +0800

    modules: 删除传输列表

--- src/views/transport-list/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <breadcrumb :items="['menu.transportlist']" />
-  </div>
-</template>
-
-<script lang="ts" setup>
-import breadcrumb from '@/components/breadcrumb.vue'
-</script>
