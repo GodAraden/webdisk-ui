36875e98bba5d964dfbba5dbbfa60b8cec8102ad
Araden <2713886816@qq.com>
2023-06-07 15:03:34 +0800

    optimize: 对话框的聚焦

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,10 +1,11 @@
-import { h, ref } from 'vue'
+import { h, nextTick, ref } from 'vue'
 import { Input, Modal } from '@arco-design/web-vue'
 import { i18n } from '@/locale'
 
 export function openDialog(type: string, defaultValue = '') {
   return new Promise<string>((resolve) => {
     const input = ref(defaultValue)
+    const inputElement = ref()
     const { close } = Modal.info({
       title: i18n.global.t(`dialog.${type}.title`),
       content: () =>
@@ -18,7 +19,7 @@ export function openDialog(type: string, defaultValue = '') {
               resolve(input.value)
               close()
             },
-            'input-attrs': { autofocus: 'autofocus' },
+            ref: inputElement,
             style: {
               'background-color': 'var(--color-menu-light-bg)',
               'border-color': 'transparent',
@@ -29,6 +30,10 @@ export function openDialog(type: string, defaultValue = '') {
       onOk: () => resolve(input.value),
       // 取消的话也不至于报错，就不用 reject 了
       onCancel: () => resolve('')
+    })
+    nextTick(() => {
+      console.log(inputElement.value)
+      inputElement.value?.focus()
     })
   })
 }
