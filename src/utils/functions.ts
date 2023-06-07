2fd73cd85eefc6cf6193a684104582b416261b22
Araden <2713886816@qq.com>
2023-06-07 14:46:20 +0800

    chore: 对话框自动聚焦、回车确认

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -4,16 +4,21 @@ import { i18n } from '@/locale'
 
 export function openDialog(type: string, defaultValue = '') {
   return new Promise<string>((resolve) => {
-    const filename = ref(defaultValue)
-    Modal.info({
+    const input = ref(defaultValue)
+    const { close } = Modal.info({
       title: i18n.global.t(`dialog.${type}.title`),
       content: () =>
         h('div', [
           h('p', { class: 'mb-2' }, i18n.global.t(`dialog.${type}.tips`)),
           h(Input, {
-            'model-value': filename.value,
+            'model-value': input.value,
             placeholder: i18n.global.t(`dialog.${type}.placeholder`),
-            onInput: (newVal) => (filename.value = newVal),
+            onInput: (newVal) => (input.value = newVal),
+            onPressEnter: () => {
+              resolve(input.value)
+              close()
+            },
+            'input-attrs': { autofocus: 'autofocus' },
             style: {
               'background-color': 'var(--color-menu-light-bg)',
               'border-color': 'transparent',
@@ -21,7 +26,7 @@ export function openDialog(type: string, defaultValue = '') {
             }
           })
         ]),
-      onOk: () => resolve(filename.value),
+      onOk: () => resolve(input.value),
       // 取消的话也不至于报错，就不用 reject 了
       onCancel: () => resolve('')
     })
