d174b228e6ce3ffc8fc23c89e34916fe0df25397
Araden <2713886816@qq.com>
2023-06-07 11:41:52 +0800

    fix: 上传文件会到根目录

--- src/views/file-list/components/footer.vue
+++ src/views/file-list/components/footer.vue
@@ -1,55 +1,52 @@
 <template>
-  <a-affix>
-    <a-row :align="'center'" class="mb-1" :gutter="6">
-      <a-col flex="100px">
-        {{ $t('filelist.footer.total', { total: renderData.length }) }}
-      </a-col>
-      <a-col flex="auto"> </a-col>
+  <a-row :align="'center'" class="mb-1" :gutter="6">
+    <a-col flex="100px">
+      {{ $t('filelist.footer.total', { total: renderData.length }) }}
+    </a-col>
+    <a-col flex="auto"> </a-col>
+    <a-col flex="1px">
+      <a-button
+        size="small"
+        shape="round"
+        type="dashed"
+        @click="onCreateFolder"
+      >
+        <template #icon>
+          <icon-folder-add />
+        </template>
+        {{ $t('filelist.footer.newFolder') }}
+      </a-button>
+    </a-col>
+    <a-col flex="1px">
+      <a-upload @before-upload="onUploadFile" directory>
+        <template #upload-button>
+          <a-button size="small" type="outline" shape="round">
+            <template #icon>
+              <icon-folder />
+            </template>
+            {{ $t('filelist.footer.uploadFolder') }}
+          </a-button>
+        </template>
+      </a-upload>
+    </a-col>
 
-      <a-col flex="1px">
-        <a-button
-          size="small"
-          shape="round"
-          type="dashed"
-          @click="onCreateFolder"
-        >
-          <template #icon>
-            <icon-folder-add />
-          </template>
-          {{ $t('filelist.footer.newFolder') }}
-        </a-button>
-      </a-col>
-      <a-col flex="1px">
-        <a-upload @before-upload="onUploadFile" directory>
-          <template #upload-button>
-            <a-button size="small" type="outline" shape="round">
-              <template #icon>
-                <icon-folder />
-              </template>
-              {{ $t('filelist.footer.uploadFolder') }}
-            </a-button>
-          </template>
-        </a-upload>
-      </a-col>
-
-      <a-col flex="1px">
-        <a-upload @before-upload="onUploadFile" multiple>
-          <template #upload-button>
-            <a-button size="small" type="primary" shape="round">
-              <template #icon>
-                <icon-upload />
-              </template>
-              {{ $t('filelist.footer.upload') }}
-            </a-button>
-          </template>
-        </a-upload>
-      </a-col>
-    </a-row>
-  </a-affix>
+    <a-col flex="1px">
+      <a-upload @before-upload="onUploadFile" multiple>
+        <template #upload-button>
+          <a-button size="small" type="primary" shape="round">
+            <template #icon>
+              <icon-upload />
+            </template>
+            {{ $t('filelist.footer.upload') }}
+          </a-button>
+        </template>
+      </a-upload>
+    </a-col>
+  </a-row>
 </template>
 
 <script lang="ts" setup>
-import { provideFileList } from '../hooks/useFileList'
+import { useFileList } from '../hooks/useFileList'
 
-const { onUploadFile, renderData, onCreateFolder } = provideFileList()
+const { onUploadFile, renderData, onCreateFolder } = useFileList()
 </script>
